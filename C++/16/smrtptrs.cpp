d5e712909fc73bc9d42fcb5e640a7c9156b5d34f
davidLeung <leungdavid989@gmail.com>
2018-01-22 11:16:29 +0800

    backup

---
+++ C++/16/smrtptrs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class Report
+{
+private:
+	string str;
+public:
+	Report(const string s) : str(s)
+	{
+		cout << "Object created!\n";
+	}
+	~Report()
+	{
+		cout << "Object delete!\n";
+	}
+	void comment() const { cout << str << "\n";}
+
+};
+
+int main()
+{
+	{
+		auto_ptr<Report> ps (new Report("using auto_ptr"));
+		ps->comment();
+	}
+	{
+		shared_ptr<Report> ps (new Report("using shared_ptr"));
+		ps->comment();
+	}
+	{
+		unique_ptr<Report> ps (new Report("using unique_ptr"));
+		ps->comment();
+	}
+	return 0;
+
+}
---
+++ C++/16/strfile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+int main()
+{
+	using namespace std;
+	ifstream fin;
+	fin.open("tobuy.txt");
+	if (fin.is_open() == false)
+	{
+		cerr << "Can't open file. Bye.\n";
+		exit(EXIT_FAILURE);
+	}
+	else
+		cout << "open file successful!\n";
+	string item;
+	int count = 0;
+	getline(fin, item, ':');
+	while (fin)
+	{
+		++count;
+		cout << count << ": " << item << endl;
+		getline(fin, item, ':');
+	}
+	cout << "Done\n";
+	fin.close();
+
+
+
+	return 0;
+}
